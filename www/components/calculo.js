d844bbb5e5db0999216974482f1b28c75cbe4378
jussimar <jussimar@jussimarleal.com.br>
2018-07-08 04:33:21 +0900

    inserindo o botão e função para subtração

--- www/components/calculo.js
+++ www/components/calculo.js
@@ -7,3 +7,11 @@ $(document).on("click","#somar", function(){
     
     $("#result").val(result);
 });
+$(document).on("click","#subtrair", function(){
+    var valor1 = $("#valor1").val();
+    var valor2 = $("#valor2").val();
+    
+    var result = parseInt(valor1) - parseInt(valor2);
+    
+    $("#result").val(result);
+});
